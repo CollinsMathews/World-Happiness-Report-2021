4a42bc081420c490588cf199ff947d82e62c9862
Collins Mathews <cmat0007@student.monash.edu>
2021-10-10 21:01:02 +1100

    Added initial design of new vis

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-var spec2 = "js/map.vg.json";
-  vegaEmbed('#vega_map', spec2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
+var spec1 = "js/world_happiness_map.vg.json";
+vegaEmbed('#vega_map', spec1).then(function (result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec2 = "js/happiness_scatter_plot.vg.json";
+vegaEmbed('#vega_scatter_plot', spec2, { "actions": false }).then(function (result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
