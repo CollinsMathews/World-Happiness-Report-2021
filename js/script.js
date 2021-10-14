cc2ce4e8e17feb807b0c9e41853c4b6dae20371b
Collins Mathews <cmat0007@student.monash.edu>
2021-10-14 16:59:39 +1100

    Added more visualisation and updated design

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ var spec2 = "js/happiness_scatter_plot.vg.json";
 vegaEmbed('#vega_scatter_plot', spec2, { "actions": false }).then(function (result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var spec3 = "js/country_bar_chart.vg.json";
+vegaEmbed('#vega_bar_chart', spec3, { "actions": false }).then(function (result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec4 = "js/parallel_coordinate_plot.vg.json";
+vegaEmbed('#parallel_coordinate_plot', spec4, { "actions": false }).then(function (result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
