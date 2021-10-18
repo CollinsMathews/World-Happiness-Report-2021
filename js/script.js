3753059c45628e0f77b62c164580fa251075511c
Collins Mathews <cmat0007@student.monash.edu>
2021-10-18 16:24:12 +1100

    Final changes of data vis

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ vegaEmbed('#vega_scatter_plot', spec2, { "actions": false }).then(function (resu
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var spec3 = "js/country_bar_chart.vg.json";
-vegaEmbed('#vega_bar_chart', spec3, { "actions": false }).then(function (result) {
-  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+// var spec3 = "js/country_bar_chart.vg.json";
+// vegaEmbed('#vega_bar_chart', spec3, { "actions": false }).then(function (result) {
+//   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
 
 var spec4 = "js/parallel_coordinate_plot.vg.json";
 vegaEmbed('#parallel_coordinate_plot', spec4, { "actions": false }).then(function (result) {
